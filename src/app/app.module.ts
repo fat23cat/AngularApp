6c71bdf5abff1bd669cdedfe491d266bfe855b0a
Pavel Sidorenko <pavel_sidorenko@epam.com>
2018-04-22 22:24:25 +0300

    fixed css, added material ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 
-import { AppComponent } from './app.component';
-import { UserWidgetComponent } from './user-widget/user-widget.component';
-import { UserComponent } from './user/user.component';
+import {AppComponent} from './app.component';
+import {UserWidgetComponent} from './user-widget/user-widget.component';
+import {UserComponent} from './user/user.component';
 import {HttpClientModule} from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
@@ -18,9 +19,11 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
